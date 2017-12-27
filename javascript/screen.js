b250294bba51a18bc1c5324e3bc0151841cb4108
Habib Aroua <HabibAroua@users.noreply.github.com>
2017-12-27 11:16:24 +0100

    dev reset and startSlide function

--- javascript/screen.js
+++ javascript/screen.js
@@ -1,1 +1,22 @@
 //Image screen
+let sliderImage = document.querySelectorAll('.slide'),
+arrowLeft=document.querySelectorAll('#arrow-left'),
+arrowRight=document.querySelectorAll('#arrow-right'),
+current = 0;
+
+//clear all images
+function reset()
+{
+	for(let i=0; i<sliderImage ; i++ )
+	{
+		sliderImage[i].style.display = 'none' ;
+	}
+}
+
+function startSlide()
+{
+	reset();
+}
+
+startSlide();
+
